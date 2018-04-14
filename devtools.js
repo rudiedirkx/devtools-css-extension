5003a198b1c7dd667b670d05b9f71111fe624540
Rudie Dirkx <github@hotblocks.nl>
2018-04-14 23:06:17 +0200

    fixes4

--- devtools.js
+++ devtools.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest;
-xhr.open("GET", "/fixes2.css", true);
+xhr.open("GET", "/fixes4.css", true);
 xhr.onload = function(e) {
 	chrome.devtools.panels.applyStyleSheet(this.responseText);
 	// console.log(this.responseText);
